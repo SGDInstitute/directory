30d121f6309ea150153c8340a2fc52c1c05c2cf0
Andrea Swick <andrea@swicksolution.com>
2016-09-01 10:05:49 -0500

    Add email

---
+++ updates/builder_table_update_sgdinstitute_directory_human_7.php
@@ -0,0 +1,23 @@
+<?php namespace SGDInstitute\Directory\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSgdinstituteDirectoryHuman7 extends Migration
+{
+    public function up()
+    {
+        Schema::table('sgdinstitute_directory_human', function($table)
+        {
+            $table->string('email')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sgdinstitute_directory_human', function($table)
+        {
+            $table->dropColumn('email');
+        });
+    }
+}
